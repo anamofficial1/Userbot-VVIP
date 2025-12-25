41b3f824632c73eb7369bb89797d58ebc756aeb2
Team Anonymous-[TZY] <anamtradervx@gmail.com>
2025-12-25 11:31:04 +0700

    Settings Owner

---
+++ settings.py
@@ -0,0 +1,13 @@
+# === TELEGRAM OWNER ===
+OWNER_ID = 7772847824
+PP = "https://files.catbox.moe/l82q8c.jpg"
+BOT_TOKEN = "8319774742:AAH0VGMsXR0I0BDZ1HQp7tPvmAJhWW78MwY"
+# === PTERODACTYL PANEL ===
+DOMAIN = "https://domain.my.id"
+PLTA = "ptla_"
+PLTC = "ptlc_"
+LOC = 1
+EGGS = 15
+# === TELEGRAM API (my.telegram.org) ===
+API_ID =  28399349 # ganti dengan api yang sesuai 
+API_HASH = "c97b28f1421e4e9bd443bf297d18b099" # ----"-----
